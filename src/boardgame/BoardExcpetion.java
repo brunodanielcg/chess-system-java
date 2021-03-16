40dc2440af577809f05b128c5f44fb24dddaabdd
brunodanielcg <bgonvalves.br@gmail.comgit config --global user.email bgonvalves.br@gmail.com>
2021-03-16 16:42:35 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardExcpetion.java
@@ -0,0 +1,11 @@
+package boardgame;
+
+public class BoardExcpetion extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExcpetion(String msg) {
+		super(msg);
+	}
+
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,7 +29,7 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		board.placePiece(new Rook(board,Color.WHITE), new Position(0,0));
-		board.placePiece(new Rook(board,Color.WHITE), new Position(0,7));
+		board.placePiece(new Rook(board,Color.WHITE), new Position(0,0));
 		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
 
 	}
