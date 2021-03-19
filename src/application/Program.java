dd6943e699f455f3af8d1f53c90d42284d417181
brunodanielcg <bgonvalves.br@gmail.comgit config --global user.email bgonvalves.br@gmail.com>
2021-03-19 17:17:17 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,15 +30,14 @@ public class Program {
 			
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
-			catch (InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
