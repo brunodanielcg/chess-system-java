c3b0c8235ba03d0805b48645677befde7d094c80
brunodanielcg <bgonvalves.br@gmail.comgit config --global user.email bgonvalves.br@gmail.com>
2021-03-19 16:50:59 -0300

    Handling exceptions and clearing screen

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -20,18 +20,24 @@ public class Board {
 	public int getColumns() {
 		return columns;
 	}
+	
+	
 	public Piece piece(int row, int column) {
 		if (!positionExists (row, column)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
+	
+	
 	public Piece piece(Position position) {
 		if (!positionExists (position)) {
 			throw new BoardException("Position not on the board");
 		}		
 		return pieces[position.getRow()][position.getColumn()];
 	}	
+	
+	
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
@@ -56,9 +62,13 @@ public class Board {
 	private boolean positionExists (int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}	
+	
+	
 	public boolean positionExists (Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}	
+	
+	
 	public boolean thereIsAPiece (Position position) {
 		if (!positionExists (position)) {
 			throw new BoardException("Position not on the board");
