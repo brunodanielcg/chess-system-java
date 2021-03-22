2baca3570948de5cf9b463f461b5abad528bf187
brunodanielcg <bgonvalves.br@gmail.comgit config --global user.email bgonvalves.br@gmail.com>
2021-03-22 10:26:00 -0300

    Commit com erro

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,28 +19,28 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List <ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try { 
-			UI.clearScreen();
-			UI.printMatch(chessMatch, captured);
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			boolean [][] possibleMoves = chessMatch.possibleMoves(source);
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces(), possibleMoves);
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
-			if (capturedPiece != null) {
-				captured.add(capturedPiece);
+				UI.clearScreen();
+				UI.printMatch(chessMatch, captured);
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean [][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}			
 			}
 			
-			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
@@ -50,5 +50,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
